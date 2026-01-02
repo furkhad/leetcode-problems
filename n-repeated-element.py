87049903cf22a7c96b7381d215b16bd62221bcc6
furkhadahmed <furkhadahmed1@gmail.com>
2026-01-02 07:21:26 +0530

    Add solution for LeetCode 961: N-Repeated Element
    Implement solution for finding N-repeated element in array.

---
+++ n-repeated-element.py
@@ -0,0 +1,10 @@
+# LeetCode 961: N-Repeated Element in Size 2N Array
+# Problem Link: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
